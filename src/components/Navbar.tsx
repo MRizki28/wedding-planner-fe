14985741419c604f66aad5fa2192716757599c93
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-02 21:58:01 +0800

    feat(home):create hero

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/img/logo.svg';
 
 export default function Navbar() {
     return (
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white dark:bg-gray-900  w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <LazyLoadImage src={logo} className="h-8" alt="logo" />
