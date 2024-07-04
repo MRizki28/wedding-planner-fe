476713449338e66914f4657adf1c6165d20ecd3b
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-04 16:08:29 +0800

    feat(venue):create venue

--- src/ui/home/Home.tsx
+++ src/ui/home/Home.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Hero } from "./components/Hero";
 import Navbar from "../../components/Navbar";
+import { Vanue } from "./components/Venue";
 
 const Home: React.FC = () => {
     return (
         <>
             <Navbar></Navbar>
-            <div className="bg-[#F4F4F4]">
+            <div className="">
                 <div className="max-w-[1140px] mx-auto">
                     <Hero></Hero>
                 </div>
             </div>
-
+            <div className="mt-52">
+                <div className="max-w-[1140px] mx-auto">
+                    <Vanue></Vanue>
+                </div>
+            </div>
         </>
     )
 }
